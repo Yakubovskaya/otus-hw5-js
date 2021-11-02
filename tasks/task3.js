43c49b19f07a79ca3f3a880e7d866fca690a1858
Marina <maribars93@yandex.ru>
2021-11-02 23:08:54 +0300

    Refact: optimize code in task3 file

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,7 +1,8 @@
 export function sumOfNum() {
   const num = prompt("Enter the number: ");
-  const num1 = +num[0];
-  const num2 = +num[1];
-  const num3 = +num[2];
-  return num1 + num2 + num3;
+  const sum = num
+    .split("")
+    .map((el) => Number(el))
+    .reduce((curr, pre) => curr + pre);
+  return sum;
 }
