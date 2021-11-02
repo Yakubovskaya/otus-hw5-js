ddb330b03f719418d5efa513a3b14c8f761f8b2a
Marina <maribars93@yandex.ru>
2021-11-02 17:23:19 +0300

    Add task3 & task3.test files

---
+++ tasks/task3.js
@@ -0,0 +1,7 @@
+export function sumOfNum() {
+  const num = prompt("Enter the number: ");
+  const num1 = +num[0];
+  const num2 = +num[1];
+  const num3 = +num[2];
+  return num1 + num2 + num3;
+}
---
+++ tasks/task3.test.js
@@ -0,0 +1,8 @@
+import { sumOfNum } from "./task3";
+
+describe("sum of the numbers", () => {
+  it("returns sum of the numbers entered by user", () => {
+    jest.spyOn(window, "prompt").mockImplementation(() => "123");
+    expect(sumOfNum()).toBe(6);
+  });
+});
